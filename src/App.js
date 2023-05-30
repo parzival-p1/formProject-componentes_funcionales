6aac4db8390a3680c167874dfc39d7b00a654ba4
Paco Hdez <francj.hdez@gmail.com>
2023-05-30 12:37:41 -0600

    agregando Container de MUI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
+import Container from '@mui/material/Container'
 import FormSignUp from './FormSignUp';
 
 function App() {
   return (
-    <>
+    <Container component="section" maxWidth="sm">
       <h1>Formulario Registro</h1>
       <FormSignUp />
-    </>
+    </Container>
   );
 }
 
